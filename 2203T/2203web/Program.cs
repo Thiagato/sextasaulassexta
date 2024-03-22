89e63262af4845d1ec7121298c844fc36286d9f1
Auto Logon <autologon@CSED.LAB>
2024-03-22 11:39:06 -0300

    terefa

--- 2203T/2203web/Program.cs
+++ 2203T/2203web/Program.cs
@@ -1,13 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<Produto> produtos = new List<Produto>(); 
+produtos.Add(new Produto("Celular", "XIOAMI"));
+produtos.Add(new Produto("Celular", "IOS"));
+produtos.Add(new Produto("Celular", "SANSUNG"));
+produtos.Add(new Produto("Celular", "SLA"));
+produtos.Add(new Produto("Celular", "ANDROID"));
+
 //endpoints - funcionalidades
 
 //GET:localhost/x/
 app.MapGet("/", () => "jEAN NO HAIR");
 //GET:localhost/x/api
-app.MapGet("/api", () => "ola");
+app.MapGet("/api/produto", () => produtos);
 //POST;localhost/x/api/taligas
-app.MapPost("/api/taligas/", () => "tilgas");
+app.MapPost("/api/produto/lisar", () => "tilgas");
 
 app.Run();
+
+record Produto(string Nome, string Descricao);
